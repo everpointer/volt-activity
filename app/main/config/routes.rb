7ae4781546890d66bc73da07bc0410ba82adcb77
everpointer <vane.zhangfeng@gmail.com>
2014-12-19 09:28:15 +0800

    initial commit for gift app using Volt Framework

---
+++ app/main/config/routes.rb
@@ -0,0 +1,13 @@
+# See https://github.com/voltrb/volt#routes for more info on routes
+
+get '/about', _action: 'about'
+
+# Routes for login and signup, provided by user-templates component gem
+get '/signup', _controller: 'user-templates', _action: 'signup'
+get '/login', _controller: 'user-templates', _action: 'login'
+
+get "/activities/{{ _index }}", _action: 'activity'
+get "/activities/{{ _index }}/share", _action: 'share'
+# The main route, this should be last. It will match any params not
+# previously matched.
+get '/', {}
---
+++ app/main/controllers/main_controller.rb
@@ -0,0 +1,54 @@
+# By default Volt generates this controller for your Main component
+class MainController < Volt::ModelController
+  model :store
+
+  def index
+    # Add code for when the index view is loaded
+    Document.ready? do
+      %x{
+        $('#scratch_pad').wScratchPad({
+          size        : 10,
+          fg          : '#cacaca',
+          bg          : '/assets/images/winner.png',
+          cursor      : 'crosshair',
+          scratchMove : function(e, percent) {
+            if(percent > 60) this.clear();
+          }
+        });
+      }
+    end
+  end
+
+  def about
+    # Add code for when the about view is loaded
+  end
+
+  def activity
+    # activity index
+  end
+
+  def share
+    # handle activity shared path
+    _activity_visits << {owner: params._owner, activity_index: params._index}
+  end
+
+  private
+
+  # The main template contains a #template binding that shows another
+  # template.  This is the path to that template.  It may change based
+  # on the params._controller and params._action values.
+  def main_path
+    params._controller.or('main') + '/' + params._action.or('index')
+  end
+
+  # Determine if the current nav component is the active one by looking
+  # at the first part of the url against the href attribute.
+  def active_tab?
+    url.path.split('/')[1] == attrs.href.split('/')[1]
+  end
+
+  # activity path for current user
+  def activity_share_path
+    url_for(action: 'share', index: 1, owner: Volt.user._email)
+  end
+end
